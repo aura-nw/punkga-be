afd384d4b47c0d2ab16fab639206b067374bd4c7
harisato <pp.quochai@gmail.com>
2023-07-10 14:08:31 +0700

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(expressCtx);
+  app.enableCors();
 
   // setup swagger
   const config = new DocumentBuilder()
