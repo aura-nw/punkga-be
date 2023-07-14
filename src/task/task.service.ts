f03ff37ffd10d6698ff77f5467e4e2a00ba966f5
harisato <pp.quochai@gmail.com>
2023-07-14 15:09:16 +0700

    feat(be): optimize count view

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -16,50 +16,65 @@ export class TasksService {
   @Cron(CronExpression.EVERY_5_MINUTES)
   async updateViews() {
     // set chapter to set
-    const chapters = await this.redisClientService.client.sMembers(
-      ['punkga', 'dev', 'chapters'].join(':'),
+    const chapters = await this.redisClientService.client.sPop(
+      [
+        this.configService.get<string>('app.name'),
+        this.configService.get<string>('app.env'),
+        'chapters',
+      ].join(':'),
+      10,
     );
-    console.log(chapters);
-    const views = await Promise.all(
-      chapters.map((chapterId: string) => {
-        // get chapter view
-        return this.redisClientService.client.getDel(
-          ['punkga', 'dev', 'chapter', chapterId.toString(), 'view'].join(':'),
-        );
-      }),
-    );
+    if (chapters.length > 0) {
+      console.log(chapters);
+      const views = await Promise.all(
+        chapters.map((chapterId: string) => {
+          // get chapter view
+          return this.redisClientService.client.getDel(
+            [
+              this.configService.get<string>('app.name'),
+              this.configService.get<string>('app.env'),
+              'chapter',
+              chapterId.toString(),
+              'view',
+            ].join(':'),
+          );
+        }),
+      );
 
-    const updates = chapters.map((chapterId: string, index: number) => ({
-      where: {
-        id: {
-          _eq: chapterId,
-        },
-      },
-      _inc: {
-        views: Number(views[index]),
-      },
-    }));
+      const updates = chapters
+        .map((chapterId: string, index: number) => ({
+          where: {
+            id: {
+              _eq: chapterId,
+            },
+          },
+          _inc: {
+            views: Number(views[index]),
+          },
+        }))
+        .filter((u) => u._inc.views !== 0);
 
-    await this.graphqlSvc.query(
-      this.configService.get<string>('graphql.endpoint'),
-      '',
-      `mutation UpdateChapterViews($chapters: [chapters_insert_input!] = {id: 10, views: 10, manga_id: 10}, $updates: [chapters_updates!] = {where: {id: {_eq: 10}}, _set: {views: 10}}) {
+      await this.graphqlSvc.query(
+        this.configService.get<string>('graphql.endpoint'),
+        '',
+        `mutation UpdateChapterViews($chapters: [chapters_insert_input!] = {id: 10, views: 10, manga_id: 10}, $updates: [chapters_updates!] = {where: {id: {_eq: 10}}, _set: {views: 10}}) {
         update_chapters_many(updates: $updates) {
           affected_rows
         }
       }
       `,
-      'UpdateChapterViews',
-      {
-        updates,
-      },
-      {
-        'x-hasura-admin-secret': this.configService.get<string>(
-          'graphql.adminSecret',
-        ),
-      },
-    );
+        'UpdateChapterViews',
+        {
+          updates,
+        },
+        {
+          'x-hasura-admin-secret': this.configService.get<string>(
+            'graphql.adminSecret',
+          ),
+        },
+      );
 
-    this.logger.debug(updates);
+      this.logger.debug('Update chapter views', updates);
+    }
   }
 }
