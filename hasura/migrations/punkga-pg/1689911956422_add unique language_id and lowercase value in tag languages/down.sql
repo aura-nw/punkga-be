38b4161b0bbbff1d720198ad9d409b825b77ef27
harisato <pp.quochai@gmail.com>
2023-07-21 10:59:48 +0700

    feat(hasura): add unique lowercase value

---
+++ hasura/migrations/punkga-pg/1689911956422_add unique language_id and lowercase value in tag languages/down.sql
@@ -0,0 +1,8 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- DROP TRIGGER IF EXISTS lowercase_tag_lang_value_on_insert ON tag_languages;
+--
+-- DROP FUNCTION IF EXISTS lowercase_tag_lang_value_on_insert;
+--
+-- CREATE UNIQUE INDEX IF NOT EXISTS tag_languagues_language_id_lower_value_key
+--    ON tag_languages (lower(value), language_id);
---
+++ hasura/migrations/punkga-pg/1689911956422_add unique language_id and lowercase value in tag languages/up.sql
@@ -0,0 +1,6 @@
+DROP TRIGGER IF EXISTS lowercase_tag_lang_value_on_insert ON tag_languages;
+
+DROP FUNCTION IF EXISTS lowercase_tag_lang_value_on_insert;
+
+CREATE UNIQUE INDEX IF NOT EXISTS tag_languagues_language_id_lower_value_key 
+   ON tag_languages (lower(value), language_id);
