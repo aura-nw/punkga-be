e388214f7e8b435669be878e085071f812de6dd3
harisato <pp.quochai@gmail.com>
2023-07-26 16:41:39 +0700

    feat(hasura): sort by latest chapter publish

---
+++ hasura/migrations/punkga-pg/1690364416444_create manga latest publish function/down.sql
@@ -0,0 +1,11 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION latest_published(manga_row manga) RETURNS
+-- timestamp with time zone
+-- AS $$
+--     SELECT pushlish_date
+--     FROM chapters
+--     WHERE chapters.manga_id = manga_row.id AND chapters.status = 'Published'
+-- 	ORDER BY pushlish_date DESC NULLS LAST
+-- 	LIMIT 1
+-- $$ LANGUAGE sql STABLE;
---
+++ hasura/migrations/punkga-pg/1690364416444_create manga latest publish function/up.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE FUNCTION latest_published(manga_row manga) RETURNS 
+timestamp with time zone 
+AS $$
+    SELECT pushlish_date
+    FROM chapters
+    WHERE chapters.manga_id = manga_row.id AND chapters.status = 'Published'
+	ORDER BY pushlish_date DESC NULLS LAST
+	LIMIT 1
+$$ LANGUAGE sql STABLE;
