3eade4d6d3a2f0f852d6d32daf642e150485f40e
harisato <pp.quochai@gmail.com>
2023-07-28 14:50:39 +0700

    feat(hasura): update api

---
+++ hasura/migrations/punkga-pg/1690529346372_add function nearest upcoming date in manga/down.sql
@@ -0,0 +1,11 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE FUNCTION nearest_upcoming(manga_row manga) RETURNS
+-- timestamp with time zone
+-- AS $$
+--     SELECT pushlish_date
+--     FROM chapters
+--     WHERE chapters.manga_id = manga_row.id AND chapters.status = 'Upcoming'
+-- 	ORDER BY pushlish_date ASC NULLS LAST
+-- 	LIMIT 1
+-- $$ LANGUAGE sql STABLE;
---
+++ hasura/migrations/punkga-pg/1690529833409_add function nearest upcoming date in manga/up.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE FUNCTION nearest_upcoming(manga_row manga) RETURNS 
+timestamp with time zone 
+AS $$
+    SELECT pushlish_date
+    FROM chapters
+    WHERE chapters.manga_id = manga_row.id AND chapters.status = 'Upcoming' AND chapters.pushlish_date > now()
+	ORDER BY pushlish_date ASC NULLS LAST
+	LIMIT 1
+$$ LANGUAGE sql STABLE;
