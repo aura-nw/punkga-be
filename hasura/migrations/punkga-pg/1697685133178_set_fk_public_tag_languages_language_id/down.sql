a89c9ff5b604cfad2d7d286781dbe5fcdfbea8f4
harisato <pp.quochai@gmail.com>
2023-10-19 10:23:39 +0700

    feat(hasura): update all foreign key to cascade

--- hasura/migrations/punkga-pg/1697685133178_set_fk_public_tag_languages_language_id/down.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."tag_languages" drop constraint "tag_languages_language_id_fkey",
-  add constraint "tag_languagues_language_id_fkey"
-  foreign key ("language_id")
-  references "public"."languages"
-  ("id") on update restrict on delete restrict;
--- hasura/migrations/punkga-pg/1697685138859_set_fk_public_tag_languages_tag_id/down.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."tag_languages" drop constraint "tag_languages_tag_id_fkey",
-  add constraint "tag_languagues_tag_id_fkey"
-  foreign key ("tag_id")
-  references "public"."tags"
-  ("id") on update restrict on delete restrict;
