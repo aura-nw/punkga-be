17a91411d7a513d913ef6c020185187d6684bce9
harisato <pp.quochai@gmail.com>
2023-10-24 16:43:01 +0700

    feat (hasura): add user xp rank

---
+++ hasura/migrations/punkga-pg/1698139978204_create_view_user_xp_rank/down.sql
@@ -0,0 +1,9 @@
+-- Could not auto-generate a down migration.
+-- Please write an appropriate down migration for the SQL below:
+-- CREATE OR REPLACE VIEW user_xp_rank AS
+--   select
+--   user_id,
+--   xp,
+--   level,
+--   rank() over (order by xp desc, updated_at asc) as rank
+-- from user_level;
---
+++ hasura/migrations/punkga-pg/1698139978204_create_view_user_xp_rank/up.sql
@@ -0,0 +1,7 @@
+CREATE OR REPLACE VIEW user_xp_rank AS
+  select
+  user_id,
+  xp,
+  level,
+  rank() over (order by xp desc, updated_at asc) as rank
+from user_level;
