d98759e09f53d6fd6510ab0629b1deb9add41109
harisato <pp.quochai@gmail.com>
2023-12-13 16:00:46 +0700

    fix(be): update import module

--- src/modules/campaign/campaign.module.ts
+++ src/modules/campaign/campaign.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+
 import { GraphqlModule } from '../graphql/graphql.module';
-import { CampaignService } from './campaign.service';
-import { CampaignGraphql } from './campaign.graphql';
-import { CampaignController } from './campaign.controller';
+import { LevelingModule } from '../leveling/leveling.module';
 import { QuestModule } from '../quest/quest.module';
 import { UserModule } from '../user/user.module';
+import { CampaignController } from './campaign.controller';
+import { CampaignGraphql } from './campaign.graphql';
+import { CampaignService } from './campaign.service';
+import { CampaignRewardService } from './reward.service';
+import { UserLevelModule } from '../user-level/user-level.module';
+import { UserWalletModule } from '../user-wallet/user-wallet.module';
 
 @Module({
-  imports: [JwtModule, GraphqlModule, QuestModule, UserModule],
-  providers: [CampaignService, CampaignGraphql],
+  imports: [JwtModule, GraphqlModule, QuestModule, UserModule, LevelingModule, UserLevelModule, UserWalletModule],
+  providers: [CampaignService, CampaignGraphql, CampaignRewardService],
   controllers: [CampaignController],
 })
-export class CampaignModule {}
+export class CampaignModule { }
