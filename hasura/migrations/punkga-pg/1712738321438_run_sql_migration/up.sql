bbd97811adcc9c0ae40f836bc00afe1653f15ce4
harisato <pp.quochai@gmail.com>
2024-04-10 15:41:46 +0700

    feat: computed user wallet addresss

---
+++ hasura/migrations/punkga-pg/1712738321438_run_sql_migration/up.sql
@@ -0,0 +1,9 @@
+CREATE OR REPLACE FUNCTION main_wallet_address(user_row authorizer_users) RETURNS TEXT as
+$$
+    SELECT
+        CASE
+            WHEN user_row.wallet_address = '' THEN (SELECT address FROM user_wallet WHERE user_id = user_row.id)
+            WHEN user_row.wallet_address IS NULL THEN (SELECT address FROM user_wallet WHERE user_id = user_row.id)
+            ELSE user_row.wallet_address
+        END AS main_address;
+$$ language sql STABLE;
