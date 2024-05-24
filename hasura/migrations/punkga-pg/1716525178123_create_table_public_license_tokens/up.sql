e8b5558443d7e7c1c4dfb24df1aa0f7a60162cd2
harisato <pp.quochai@gmail.com>
2024-05-24 11:42:28 +0700

    add license token api

---
+++ hasura/migrations/punkga-pg/1716525178123_create_table_public_license_tokens/up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE "public"."license_tokens" ("id" serial NOT NULL, "license_id" Text NOT NULL, "license_template_address" text NOT NULL, "term_id" text NOT NULL, "owner" bpchar NOT NULL, "ip_asset_id" text NOT NULL, "created_at" timestamptz NOT NULL DEFAULT now(), "updated_at" timestamptz NOT NULL DEFAULT now(), PRIMARY KEY ("id") );COMMENT ON TABLE "public"."license_tokens" IS E'IP Asset licenese tokens';
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_license_tokens_updated_at"
+BEFORE UPDATE ON "public"."license_tokens"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_license_tokens_updated_at" ON "public"."license_tokens"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
