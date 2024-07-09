3e94b7ecdcf5120baf4813dc9e80a01c67752572
harisato <pp.quochai@gmail.com>
2024-07-09 17:23:38 +0700

    Revert "feat: quest multi language"
    This reverts commit 8b01b40c1fab3967c16b216ba99959091ab83c3a.

--- hasura/migrations/punkga-pg/1720490689053_set_fk_public_i18n_quest_id/up.sql
+++
@@ -1,5 +0,0 @@
-alter table "public"."i18n"
-  add constraint "i18n_quest_id_fkey"
-  foreign key ("quest_id")
-  references "public"."quests"
-  ("id") on update cascade on delete cascade;
--- hasura/migrations/punkga-pg/1720492406521_alter_table_public_i18n_add_unique_quest_id_language_id/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."i18n" drop constraint "i18n_quest_id_language_id_key";
