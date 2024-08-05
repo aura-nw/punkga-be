23329f7bcb71dad8df376324b9a8b9d3772f3329
harisato <pp.quochai@gmail.com>
2024-08-05 14:13:18 +0700

    feat: import artwork with contest round

--- src/modules/artwork/artwork.service.ts
+++ src/modules/artwork/artwork.service.ts
@@ -6,6 +6,8 @@ import {
 import { ConfigService } from '@nestjs/config';
 import { parse } from 'csv-parse/sync';
 import axios from 'axios';
+import { GoogleAuth } from 'google-auth-library';
+import { google } from 'googleapis';
 
 import { ContextProvider } from '../../providers/contex.provider';
 import { FilesService } from '../files/files.service';
@@ -55,31 +57,28 @@ export class ArtworkService {
         const creatorId = insertCreatorResult.data.insert_creators_one.id;
 
         // upload image to s3
-        const crawlPromises = artworks
+        const crawlImageResult = artworks
           .filter((str) => str !== '')
-          .map((artwork: string) => {
-            let url = artwork;
-            if (artwork.indexOf('imgur') > 0) url = artwork + '.jpg';
-            if (artwork.indexOf('drive.google.com') > 0)
-              url = `https://lh3.googleusercontent.com/d/${
-                artwork.split('/')?.[5]
-              }`;
-            return this.crawlImage(url);
+          .map(async (artwork: string) => {
+            const result = await this.crawlImage(artwork);
+            return result;
           });
-        const crawlImageResult = await Promise.all(crawlPromises);
+        // const crawlImageResult = await Promise.all(crawlPromises);
 
         const s3SubFolder =
           this.configService.get<string>('aws.s3SubFolder') || 'images';
 
         await Promise.all(
-          crawlImageResult.map((image, index) => {
-            const keyName = `${s3SubFolder}/creator-${creatorId}/artworks/${contest_round}-${index}.jpg`;
-            return this.fileService.uploadToS3(
-              keyName,
-              image.buffer,
-              image.mimeType
-            );
-          })
+          crawlImageResult
+            .filter((result) => result.buffer)
+            .map((image, index) => {
+              const keyName = `${s3SubFolder}/creator-${creatorId}/artworks/${contest_round}-${index}.jpg`;
+              return this.fileService.uploadToS3(
+                keyName,
+                image.buffer,
+                image.mimeType
+              );
+            })
         );
 
         const newArtworks = artworks
@@ -111,13 +110,23 @@ export class ArtworkService {
     return creatorArtworks;
   }
 
-  private async crawlImage(url: string) {
+  private async crawlImage(artWorkUrl: string) {
+    if (artWorkUrl.indexOf('imgur') > 0) {
+      return this.crawlImgurImage(artWorkUrl + '.jpg');
+    }
+
+    if (artWorkUrl.indexOf('drive.google.com') > 0) {
+      return this.crawlGoogleDriveImage(artWorkUrl);
+    }
+  }
+
+  private async crawlImgurImage(url: string) {
     try {
       const response = await axios.get(url, {
         responseType: 'arraybuffer',
         timeout: 10000,
       });
-      const mimeType = response.headers['content-type'];
+      const mimeType = response.headers['content-type'] as string;
       const buffer = Buffer.from(response.data, 'utf-8');
 
       return {
@@ -125,8 +134,45 @@ export class ArtworkService {
         buffer,
       };
     } catch (error) {
-      console.log(error);
-      throw error;
+      return {
+        errors: {
+          message: JSON.stringify(error),
+        },
+      };
+    }
+  }
+
+  private async crawlGoogleDriveImage(url: string) {
+    const auth = new GoogleAuth({
+      scopes: 'https://www.googleapis.com/auth/drive',
+      keyFilename: this.configService.get<string>('google.analytics.keyFile'),
+    }) as any;
+    const service = google.drive({ version: 'v3', auth });
+
+    const fileId = url.split('/')?.[5];
+    try {
+      const file = (await service.files.get(
+        {
+          fileId: fileId,
+          alt: 'media',
+        },
+        {
+          responseType: 'arraybuffer',
+        }
+      )) as any;
+      const mimeType = file.headers['content-type'] as string;
+      const buffer = Buffer.from(file.data, 'utf-8');
+
+      return {
+        mimeType,
+        buffer,
+      };
+    } catch (error) {
+      return {
+        errors: {
+          message: JSON.stringify(error),
+        },
+      };
     }
   }
 }
